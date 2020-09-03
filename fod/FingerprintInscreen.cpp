bc6380a5f719db09383b255cb40162679d6b1035
Weikai Kong <pig.priv@gmail.com>
2020-09-03 15:03:58 +0200

    lmi: fod: Add support for Xiaomi's touchfeature
    Change-Id: I15a4a3d777d784abe04e88372647fc9abff78626
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

--- fod/FingerprintInscreen.cpp
+++ fod/FingerprintInscreen.cpp
@@ -28,9 +28,8 @@
 #define PARAM_NIT_630_FOD 1
 #define PARAM_NIT_NONE 0
 
-#define FOD_STATUS_PATH "/sys/devices/virtual/touch/tp_dev/fod_status"
-#define FOD_STATUS_ON 1
-#define FOD_STATUS_OFF 0
+#define Touch_Fod_Enable 10
+#define Touch_Aod_Enable 11
 
 #define FOD_SENSOR_X 543
 #define FOD_SENSOR_Y 1732
@@ -62,6 +61,7 @@ static void set(const std::string& path, const T& value) {
 }
 
 FingerprintInscreen::FingerprintInscreen() {
+    TouchFeatureService = ITouchFeature::getService();
     xiaomiDisplayFeatureService = IDisplayFeature::getService();
     xiaomiFingerprintService = IXiaomiFingerprint::getService();
 }
@@ -88,24 +88,26 @@ Return<void> FingerprintInscreen::onFinishEnroll() {
 
 Return<void> FingerprintInscreen::onPress() {
     acquire_wake_lock(PARTIAL_WAKE_LOCK, LOG_TAG);
+    TouchFeatureService->setTouchMode(Touch_Fod_Enable, 1);
     xiaomiFingerprintService->extCmd(COMMAND_NIT, PARAM_NIT_630_FOD);
     return Void();
 }
 
 Return<void> FingerprintInscreen::onRelease() {
+    TouchFeatureService->resetTouchMode(Touch_Fod_Enable);
     xiaomiFingerprintService->extCmd(COMMAND_NIT, PARAM_NIT_NONE);
     release_wake_lock(LOG_TAG);
     return Void();
 }
 
 Return<void> FingerprintInscreen::onShowFODView() {
-    set(FOD_STATUS_PATH, FOD_STATUS_ON);
+    TouchFeatureService->setTouchMode(Touch_Fod_Enable, 1);
     xiaomiDisplayFeatureService->setFeature(0, 17, 1, 255);
     return Void();
 }
 
 Return<void> FingerprintInscreen::onHideFODView() {
-    set(FOD_STATUS_PATH, FOD_STATUS_OFF);
+    TouchFeatureService->resetTouchMode(Touch_Fod_Enable);
     xiaomiDisplayFeatureService->setFeature(0, 17, 0, 255);
     return Void();
 }
