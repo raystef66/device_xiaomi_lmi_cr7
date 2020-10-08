eddd2c01eab848e986b0e0857131347f793a038a
Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>
2020-10-08 23:11:49 +0530

    lmi: Correct FOD coords
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

--- fod/FingerprintInscreen.cpp
+++ fod/FingerprintInscreen.cpp
@@ -32,9 +32,9 @@
 #define FOD_STATUS_ON 1
 #define FOD_STATUS_OFF 0
 
-#define FOD_SENSOR_X 445
-#define FOD_SENSOR_Y 1931
-#define FOD_SENSOR_SIZE 190
+#define FOD_SENSOR_X 543
+#define FOD_SENSOR_Y 1732
+#define FOD_SENSOR_SIZE 202
 
 #define BRIGHTNESS_PATH "/sys/class/backlight/panel0-backlight/brightness_clone"
 
