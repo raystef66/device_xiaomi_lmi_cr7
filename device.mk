1fc2110e7ad3e625a1a14832e8564b530c8b3444
Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>
2020-10-13 00:56:08 +0530

    lmi: system: import custom props
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

--- device.mk
+++ device.mk
@@ -133,6 +133,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.ims.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.telephony.ims.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/handheld_core_hardware.xml
 
+# Properties
+-include $(LOCAL_PATH)/system_prop.mk
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
---
+++ system_prop.mk
@@ -0,0 +1,29 @@
+# Display
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.fod.modified.dc_status=false \
+    sys.displayfeature.hbm.enable=true
+# Graphics
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.sf.disable_backpressure=1 \
+    debug.sf.enable_hwc_vds=1
+# Memory optimizations
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.qti.sys.fw.bservice_enable=true
+
+# Netflix
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.netflix.bsp_rev=Q855-16947-1
+
+# RIL
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.data.iwlan=1 \
+    persist.data.iwlan.ipsec.ap=1 \
+    persist.dbg.wfc_avail_ovr=1 \
+    persist.sys.cust.lte_config=true \
+    persist.radio.VT_ENABLE=1 \
+    persist.radio.volte.dan_support=true
+
+# RCS
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.rcs.supported=1
+
