1b02e0ac38e2879f38de18f689f1639bd33127ab
Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>
2020-10-22 19:17:48 +0530

    lmi: update system props
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

--- system_prop.mk
+++ system_prop.mk
@@ -1,13 +1,42 @@
+# Bluetooth
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.bt.a2dp.aac_whitelist=false \
+    persist.vendor.btstack.a2dp_offload_cap=sbc-aptx-aptxtws-aptxhd-aptxadaptive-aac-ldac \
+    persist.vendor.btstack.enable.splita2dp=true \
+    persist.vendor.btstack.enable.twsplus=true \
+    persist.vendor.btstack.enable.twsplussho=true \
+    ro.bluetooth.library_name=libbluetooth_qti.so \
+    vendor.bluetooth.soc=cherokee
+
+# CNE and DPM
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.cne.feature=1 \
+    persist.vendor.dpm.feature=1 \
+    persist.vendor.dpm.loglevel=0 \
+    persist.vendor.dpm.nsrm.bkg.evt=3955
+
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.fod.modified.dc_status=false \
+    ro.eyecare.brightness.threshold=11 \
+    ro.eyecare.brightness.level=5 \
+    persist.displayfeature.dc_backlight.threshold=610 \
+    persist.displayfeature.dc_backlight.enable=false \
+    ro.displayfeature.histogram.enable=true \
     sys.displayfeature.hbm.enable=true
 
 # Graphics
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.disable_backpressure=1 \
+    debug.sf.latch_unsignaled=1 \
     debug.sf.enable_hwc_vds=1 \
     debug.cpurend.vsync=false
+
+# Media
+PRODUCT_PROPERTY_OVERRIDES += \
+    media.stagefright.thumbnail.prefer_hw_codecs=true \
+    media.settings.xml=/system/etc/media_profiles_vendor.xml \
+    vendor.mm.enable.qcom_parser=63963135
 
 # Memory optimizations
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -23,10 +52,23 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.data.iwlan.ipsec.ap=1 \
     persist.dbg.wfc_avail_ovr=1 \
     persist.sys.cust.lte_config=true \
-    persist.radio.VT_ENABLE=1 \
+    persist.radio.VT_HYBRID_ENABLE=1 \
+    persist.vendor.ims.disableADBLogs=1 \
+    persist.vendor.ims.disableIMSLogs=1 \
     persist.radio.volte.dan_support=true
 
 # RCS
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.rcs.supported=1
 
+# Sensors
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.sensors.enable.mag_filter=true
+
+# Subsystem ramdump
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.ssr.restart_level=ALL_ENABLE
+
+# Vendor
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.qti.va_aosp.support=1
