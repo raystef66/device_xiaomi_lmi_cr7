116148aca7a84793e533ee373fd1865c7686aab4
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2020-10-25 11:23:00 +0530

    lmi: parts: Switch to LED breathing effect
    * Match stock behaviour
    
    Change-Id: I594cb66981d524f3f15470b153270186aa2cae9c
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

--- parts/src/org/lineageos/settings/popupcamera/PopupCameraService.java
+++ parts/src/org/lineageos/settings/popupcamera/PopupCameraService.java
@@ -210,12 +210,12 @@ public class PopupCameraService extends Service implements Handler.Callback {
 
     private void lightUp() {
         if (mPopupCameraPreferences.isLedAllowed()) {
-           FileUtils.writeLine(Constants.RED_LED_PATH, "255");
-           FileUtils.writeLine(Constants.GREEN_LED_PATH, "255");
-           FileUtils.writeLine(Constants.BLUE_LED_PATH, "255");
-           FileUtils.writeLine(Constants.RED_RIGHT_LED_PATH, "255");
-           FileUtils.writeLine(Constants.GREEN_RIGHT_LED_PATH, "255");
-           FileUtils.writeLine(Constants.BLUE_RIGHT_LED_PATH, "255");
+           FileUtils.writeLine(Constants.RED_LED_PATH, "1");
+           FileUtils.writeLine(Constants.GREEN_LED_PATH, "1");
+           FileUtils.writeLine(Constants.BLUE_LED_PATH, "1");
+           FileUtils.writeLine(Constants.RED_RIGHT_LED_PATH, "1");
+           FileUtils.writeLine(Constants.GREEN_RIGHT_LED_PATH, "1");
+           FileUtils.writeLine(Constants.BLUE_RIGHT_LED_PATH, "1");
 
             mHandler.postDelayed(() -> {
                 FileUtils.writeLine(Constants.RED_LED_PATH, "0");
@@ -224,7 +224,7 @@ public class PopupCameraService extends Service implements Handler.Callback {
                 FileUtils.writeLine(Constants.RED_RIGHT_LED_PATH, "0");
                 FileUtils.writeLine(Constants.GREEN_RIGHT_LED_PATH, "0");
                 FileUtils.writeLine(Constants.BLUE_RIGHT_LED_PATH, "0");
-           }, 1200);
+           }, 2300);
         }
     }
 
