9f130f647c6f762f04772b6c5c6ff962ce93f93f
Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>
2020-10-29 02:18:10 +0530

    lmi: build libhidltransport and libhwbinder
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

--- device.mk
+++ device.mk
@@ -84,7 +84,9 @@ PRODUCT_COPY_FILES += \
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
-    android.hidl.manager@1.0
+    android.hidl.manager@1.0 \
+    libhidltransport \
+    libhwbinder
 
 # HotwordEnrollement app permissions
 PRODUCT_COPY_FILES += \
