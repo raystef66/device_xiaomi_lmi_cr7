74004a9f4b7614e74f0e0dfb19f21db9f3dd4ac6
Official-Ayrton990 <15030201@itcelaya.edu.mx>
2020-11-08 00:46:31 -0600

    lm: BoardConfig: Simplify sepolicy
    Since we don't ship vendor, we can use sepolicy this way
    
    Signed-off-by: Official-Ayrton990 <15030201@itcelaya.edu.mx>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -174,16 +174,10 @@ TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888"
 
 # Sepolicy
+include device/qcom/sepolicy/SEPolicy.mk
 SELINUX_IGNORE_NEVERALLOWS := true
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR := $(DEVICE_PATH)/sepolicy/private
-
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/qcom/sepolicy_vndr/generic/private \
-    device/qcom/sepolicy_vndr/qva/private
-
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
-    device/qcom/sepolicy_vndr/generic/public \
-    device/qcom/sepolicy_vndr/qva/public
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Releasetools
 TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
