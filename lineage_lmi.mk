a6e87e56c0e66087fee30a9abffcc34c1eb413a1
raystef66 <s.vanbarel@gmail.com>
2021-01-03 19:11:20 +0000

    lmi: target has FOD + animations

--- lineage_lmi.mk
+++ lineage_lmi.mk
@@ -9,9 +9,15 @@ $(call inherit-product, device/xiaomi/lmi/device.mk)
 # Inherit some common aicp stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+#FOD
+# FOD
+TARGET_HAS_FOD =: true
+EXTRA_FOD_ANIMATIONS := true
+
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_lmi
 PRODUCT_DEVICE := lmi
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := F2 Pro
 PRODUCT_MANUFACTURER := Xiaomi
+
