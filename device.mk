2fc21e59c9f9a75fbae54abcc9dbe4d01f6e487f
raystef66 <s.vanbarel@gmail.com>
2021-02-02 10:49:52 +0000

    lmi: add LiveDisplay

--- device.mk
+++ device.mk
@@ -127,6 +127,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-impl.xiaomi_kona
 
+# Livedisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service-sdm
+
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_profiles_vendor.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/media_profiles_vendor.xml
